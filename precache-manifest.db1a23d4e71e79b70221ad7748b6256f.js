d2747f8d2274b3534ad8942ad666004c55fad813
Mario Barceinasº <dev.mariobarceinas@gmail.com>
2019-06-21 00:37:36 -0500

    Updates

---
+++ precache-manifest.db1a23d4e71e79b70221ad7748b6256f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a456e36a30301ce99b2bdd56547063f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0dc46d35ed9c6a5a13",
+    "url": "/static/css/main.6d09a091.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5f0dc46d35ed9c6a5a13",
+    "url": "/static/js/main.fe6152af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
